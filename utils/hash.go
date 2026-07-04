34966deb5f79d8402f1708a7e6e6dc503389e112
Shabinn-k <shhabin.07@gmail.com>
2026-07-04 10:50:40 +0530

    some errors

--- utils/hash.go
+++ utils/hash.go
@@ -5,7 +5,9 @@ import "golang.org/x/crypto/bcrypt"
 func HashPassword(password string) (string, error) {
 	byte, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(byte), err
+
 }
 func ComparePassword(hash, password string) error {
+	
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
